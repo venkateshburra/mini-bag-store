bf16c3cf1ee59701f80854662f9d21e5608fbeb9
Venkatesh Burra <burravenkatesh284@gmail.com>
2025-12-30 13:59:09 +0530

    handle React Router refresh with regex fallback route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from 'path';
 import connectDB from "./config/db.js";
 import productsRouter from "./routes/productRoutes.js";
 import cartRouter from "./routes/cartRoutes.js";
@@ -12,6 +13,9 @@ connectDB();
 
 const app = express();
 
+const __dirname = path.resolve();
+
+
 app.use(cors());
 app.use(express.json());
 
@@ -19,6 +23,19 @@ app.use(express.json());
 app.use("/api/products", productsRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/checkout", checkRouter);
+
+// 👉 Serve frontend
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+// 👉 React Router fix (MUST BE LAST)
+// React Router fallback (SAFE with Node 22)
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "frontend/dist", "index.html")
+  );
+});
+
+
 
 
 const PORT = process.env.PORT || 3000;
